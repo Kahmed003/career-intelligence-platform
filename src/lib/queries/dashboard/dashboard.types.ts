f88f8a1be788669704cbbf76775d8d611445c207
Ahmed Kabuya <akabuya@wesleyan.edu>
2026-09-15 16:32:38 -0400

    Add files via upload

---
+++ src/lib/queries/dashboard/dashboard.types.ts
@@ -0,0 +1,9 @@
+export interface CareerDashboardData {
+ generatedAt:string;
+ pipeline:unknown[];
+ deadlines:unknown[];
+ matchedOpportunities:unknown[];
+ relationshipsNeedingAttention:unknown[];
+ campaigns:unknown[];
+ pipelineHistory:unknown[];
+}
