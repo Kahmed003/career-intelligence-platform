c3dd4baf5a0e22dd4b73c5ca4017b96941824df9
Ahmed Kabuya <akabuya@wesleyan.edu>
2026-09-15 13:10:02 -0400

    Create index.ts to export service modules

---
+++ src/lib/application/services/index.ts
@@ -0,0 +1,7 @@
+export * from "./activity-ledger.service";
+export * from "./projects.service";
+export * from "./tasks.service";
+export * from "./organizations.service";
+export * from "./people.service";
+export * from "./opportunities.service";
+export * from "./applications.service";
