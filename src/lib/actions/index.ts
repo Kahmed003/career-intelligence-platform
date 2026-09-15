4fb9a8a178d9c01eeddae9baae46c486d99c2a75
Ahmed Kabuya <akabuya@wesleyan.edu>
2026-09-15 16:13:29 -0400

    Add index.ts to export action modules

---
+++ src/lib/actions/index.ts
@@ -0,0 +1,7 @@
+export * from "./core/action-result";
+export * from "./projects.actions";
+export * from "./tasks.actions";
+export * from "./organizations.actions";
+export * from "./people.actions";
+export * from "./opportunities.actions";
+export * from "./applications.actions";
